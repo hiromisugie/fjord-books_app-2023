ebeee015d8c4ae7c42de00df9ea73e8f3b8147e1
hiromisugie <lozz.gggg@gmail.com>
2023-04-28 05:22:42 +0900

    ヘッダーナビの追加と、コントローラーの更新（ログインしないとコンテンツが見られないようにする、ログイン後に一覧ページへリダイレクトする）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! # サインインしてないとアクセスできないようにする
+
+  def after_sign_in_path_for(resource)
+    books_path # サインイン後に遷移するpathを設定
+  end
 end
