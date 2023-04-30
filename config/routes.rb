5dec91a692f6a0f8f2e6f3b7b62ddd9636fb87c9
hiromisugie <lozz.gggg@gmail.com>
2023-05-01 06:03:14 +0900

    ログアウトできるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
