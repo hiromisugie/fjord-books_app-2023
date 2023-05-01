18bd82790de496a0771fb2b67328910774f02cc0
hiromisugie <lozz.gggg@gmail.com>
2023-05-02 05:19:35 +0900

    validatesの記述ミスを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates profile, length: { maximum: 500 }
+  validates: :profile, length: { maximum: 500 }
 end
