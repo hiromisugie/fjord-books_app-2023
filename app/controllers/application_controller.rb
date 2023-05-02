33021171915ddb7c1fe445a9fbb6720587cb363b
hiromisugie <lozz.gggg@gmail.com>
2023-05-03 04:34:41 +0900

    application_controller.rbの修正（アカウント作成時に追加したカラムを許可するようにした）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user! # サインインしてないとアクセスできないようにする
 
+  before_action :configure_permitted_parameters, if: :devise_controller? # deviseコントローラーにストロングパラメータを追加
+
   def after_sign_in_path_for(resource)
     books_path # ログイン後に遷移するpathを設定
   end
 
   def after_sign_out_path_for(resource)
     new_user_session_path # ログアウト後に遷移するpathを設定
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :address, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :profile]) # アカウント編集時にpostal_code、address、profileを許可
   end
 end
