51a5c82e4723021c63e937ddd90b4d91c8f78858
hiromisugie <lozz.gggg@gmail.com>
2023-07-07 05:58:00 +0900

    ルーティング、コントローラーの設定

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
