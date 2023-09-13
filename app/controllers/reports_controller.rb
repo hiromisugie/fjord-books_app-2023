b795a97dbf0d7c0923efa70ba26d09652de64061
hiromisugie <lozz.gggg@gmail.com>
2023-09-14 05:00:09 +0900

    トランザクションで日報保存と言及保存のどちらかが失敗したらロールバックするように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,27 +20,37 @@ class ReportsController < ApplicationController
   def edit; end
 
   def create
-    @report = current_user.reports.new(report_params)
-
-    if @report.save
-      add_new_mentions if @report.content.include?('http://localhost:3000/')
-      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
-    else
-      render :new, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      @report = current_user.reports.new(report_params)
+  
+      if @report.save
+        if @report.content.include?('http://localhost:3000/')
+          add_new_mentions
+        end
+        redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+      else
+        flash.now[:notice] = "mentionの保存に失敗したので、作成できませんでした。"
+        render :new, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
+      end
     end
   end
 
   def update
-    if @report.update(report_params)
-      if @report.content.include?('http://localhost:3000')
-        add_new_mentions
-        delete_mentions
+    ActiveRecord::Base.transaction do
+      if @report.update(report_params)
+        if @report.content.include?('http://localhost:3000')
+          add_new_mentions
+          delete_mentions
+        else
+          @report.mentioning_reports.clear
+        end
+        redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
       else
-        @report.mentioning_reports.clear
+        flash.now[:notice] = "mentionの保存に失敗したので、更新できませんでした。"
+        render :edit, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
       end
-      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
     end
   end
 
