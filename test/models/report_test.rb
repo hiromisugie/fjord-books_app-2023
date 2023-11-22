3f41a57c1fdbb242faede991346306d70774d0fd
hiromisugie <lozz.gggg@gmail.com>
2023-11-22 16:47:51 +0900

    rubocopの指摘を反映（ダブルクオートをシングルクオートに）

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,6 +5,6 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
   test '#created_on' do
     report = Report.new(created_at: '2023-11-22 12:00:00')
-    assert_equal 'Wed, 22 Nov 2023', report.created_on.strftime("%a, %d %b %Y")
+    assert_equal 'Wed, 22 Nov 2023', report.created_on.strftime('%a, %d %b %Y')
   end
 end
